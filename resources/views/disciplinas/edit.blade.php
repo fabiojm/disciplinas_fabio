74d73563524b2d2f5f31a48673f295d528d51d10
Fabio <fabioj.moretti@gmail.com>
2018-03-13 15:35:24 -0300

    Feito o template

--- resources/views/disciplinas/edit.blade.php
+++ resources/views/disciplinas/edit.blade.php
@@ -1,7 +1,32 @@
+@extends('master')
+
+@section('content')
 	<form method="POST" action="/disciplina/{{ $disciplina->id }}">
 		{{ csrf_field() }}
 		{{ method_field('patch') }}
-		Título: <input type="text" name="titulo" value=" {{ $disciplina->titulo }}"><br/>
-		Ementa: <textarea name="ementa">{{ $disciplina->ementa }} </textarea> <br/>
-		<button type="submit">Salvar</button>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="form-group">
+					<label for="titulo" class="for-control">Título:</label>
+					<input class="form-control" type="text" name="titulo" value="{{ $disciplina->titulo }}">
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="form-group">
+						<label for="ementa" class="for-control">Ementa:</label>
+						<textarea class="form-control" name="ementa">{{ $disciplina->ementa }}</textarea> <br/>
+				</div>
+			</div>
+		</div>
+
+
+		<button class="btn btn-primary" type="submit">Salvar</button>
+	
+
+
 	</form>
+@endsection
--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,21 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Disciplinas</title>
-</head>
-<body>
-	<a href="/disciplina/cadastrar">Cadastrar disciplina</a>
+@extends('master')
+
+@section('content')
+	<a class="btn btn-primary" href="/disciplinas/create">Cadastrar disciplina</a>
 	<ul>
 		@foreach ($disciplinas as $disciplina)
+			<div class="row form-group">
 			<li><a href="/disciplinas/{{ $disciplina->id}}"> {{$disciplina->titulo}} </a> 
 			<a href="/disciplina/{{ $disciplina->id }}/edit"> - Editar </a> 
-			<form method="POST" action="/disciplina/{{ $disciplina->id }}">
+			<form class="form-inline" method="POST" action="/disciplina/{{ $disciplina->id }}">
 				{{ csrf_field() }}
 				{{ method_field('delete') }}
-				<button type="submit"> Apagar</button>
+				<button class="btn btn-danger" type="submit"> Apagar</button>
 			</form>
 			</li>
+			</div>
 		@endforeach
 	</ul>
-</body>
-</html>
+@endsection
